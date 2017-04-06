8d85a27b7fc8be3763c695e8aaa393ccb00e270d
vrms <vrms@netcologne.de>
2017-04-06 10:14:22 +0800

    commit

--- Thumb/Stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// ScanAPIHelper.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
